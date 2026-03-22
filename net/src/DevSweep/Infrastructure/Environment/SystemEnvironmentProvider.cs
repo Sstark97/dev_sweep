9b49d9dab8988509445c8b8ec9463cbcf3b92df6
Sstark97 <aitorscinfo@gmail.com>
2026-03-22 19:25:38 +0000

    feat(infrastructure): implement stale project detection in adapters

--- net/src/DevSweep/Infrastructure/Environment/SystemEnvironmentProvider.cs
+++ net/src/DevSweep/Infrastructure/Environment/SystemEnvironmentProvider.cs
@@ -20,165 +20,109 @@ public sealed class SystemEnvironmentProvider : IEnvironmentProvider
 
     public FilePath HomePath => ToFilePath(homeDirectory);
 
-    public FilePath JetBrainsBasePath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.JetBrainsBase(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.JetBrainsBase(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.JetBrainsBase(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath JetBrainsBasePath() => ResolvePath(
+        MacOsPaths.JetBrainsBase,
+        LinuxPaths.JetBrainsBase,
+        _ => WindowsPaths.JetBrainsBase());
 
-    public FilePath JetBrainsCachePath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.JetBrainsCache(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.JetBrainsCache(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.JetBrainsCache(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath JetBrainsCachePath() => ResolvePath(
+        MacOsPaths.JetBrainsCache,
+        LinuxPaths.JetBrainsCache,
+        _ => WindowsPaths.JetBrainsCache());
 
-    public FilePath DockerConfigPath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.DockerConfig(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.DockerConfig(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.DockerConfig(homeDirectory),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath DockerConfigPath() => ResolvePath(
+        MacOsPaths.DockerConfig,
+        LinuxPaths.DockerConfig,
+        WindowsPaths.DockerConfig);
 
-    public FilePath MavenRepositoryPath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.MavenRepository(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.MavenRepository(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.MavenRepository(homeDirectory),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath MavenRepositoryPath() => ResolvePath(
+        MacOsPaths.MavenRepository,
+        LinuxPaths.MavenRepository,
+        WindowsPaths.MavenRepository);
 
-    public FilePath GradleCachePath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.GradleCache(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.GradleCache(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.GradleCache(homeDirectory),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath GradleCachePath() => ResolvePath(
+        MacOsPaths.GradleCache,
+        LinuxPaths.GradleCache,
+        WindowsPaths.GradleCache);
 
-    public FilePath NodeModulesGlobalPath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.NodeModulesGlobal(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.NodeModulesGlobal(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.NodeModulesGlobal(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath NodeModulesGlobalPath() => ResolvePath(
+        MacOsPaths.NodeModulesGlobal,
+        LinuxPaths.NodeModulesGlobal,
+        _ => WindowsPaths.NodeModulesGlobal());
 
-    public FilePath PythonCachePath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.PythonCache(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.PythonCache(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.PythonCache(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath PythonCachePath() => ResolvePath(
+        MacOsPaths.PythonCache,
+        LinuxPaths.PythonCache,
+        _ => WindowsPaths.PythonCache());
 
-    public FilePath SdkmanPath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.Sdkman(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.Sdkman(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.Sdkman(homeDirectory),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath SdkmanPath() => ResolvePath(
+        MacOsPaths.Sdkman,
+        LinuxPaths.Sdkman,
+        WindowsPaths.Sdkman);
 
-    public FilePath HomebrewCachePath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.HomebrewCache(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.HomebrewCache(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.HomebrewCache(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath HomebrewCachePath() => ResolvePath(
+        MacOsPaths.HomebrewCache,
+        LinuxPaths.HomebrewCache,
+        _ => WindowsPaths.HomebrewCache());
 
-    public FilePath GradleWrapperPath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.GradleWrapper(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.GradleWrapper(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.GradleWrapper(homeDirectory),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath GradleWrapperPath() => ResolvePath(
+        MacOsPaths.GradleWrapper,
+        LinuxPaths.GradleWrapper,
+        WindowsPaths.GradleWrapper);
 
-    public FilePath NvmCachePath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.NvmCache(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.NvmCache(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.NvmCache(homeDirectory),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath NvmCachePath() => ResolvePath(
+        MacOsPaths.NvmCache,
+        LinuxPaths.NvmCache,
+        WindowsPaths.NvmCache);
 
-    public FilePath NpmFullPath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.NpmFull(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.NpmFull(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.NpmFull(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath NpmFullPath() => ResolvePath(
+        MacOsPaths.NpmFull,
+        LinuxPaths.NpmFull,
+        _ => WindowsPaths.NpmFull());
 
-    public FilePath YarnCachePath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.YarnCache(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.YarnCache(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.YarnCache(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath YarnCachePath() => ResolvePath(
+        MacOsPaths.YarnCache,
+        LinuxPaths.YarnCache,
+        _ => WindowsPaths.YarnCache());
 
-    public FilePath PnpmStorePath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.PnpmStore(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.PnpmStore(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.PnpmStore(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath PnpmStorePath() => ResolvePath(
+        MacOsPaths.PnpmStore,
+        LinuxPaths.PnpmStore,
+        _ => WindowsPaths.PnpmStore());
 
-    public FilePath PoetryCachePath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.PoetryCache(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.PoetryCache(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.PoetryCache(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath PoetryCachePath() => ResolvePath(
+        MacOsPaths.PoetryCache,
+        LinuxPaths.PoetryCache,
+        _ => WindowsPaths.PoetryCache());
 
-    public FilePath SystemTempPath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.SystemTemp(),
-            OperatingSystemType.Linux => LinuxPaths.SystemTemp(),
-            OperatingSystemType.Windows => WindowsPaths.SystemTemp(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath SystemTempPath() => ResolvePath(
+        _ => MacOsPaths.SystemTemp(),
+        _ => LinuxPaths.SystemTemp(),
+        _ => WindowsPaths.SystemTemp());
 
-    public FilePath SystemLogsPath() => ToFilePath(
-        operatingSystem switch
-        {
-            OperatingSystemType.MacOS => MacOsPaths.SystemLogs(),
-            OperatingSystemType.Linux => LinuxPaths.SystemLogs(),
-            OperatingSystemType.Windows => WindowsPaths.SystemLogs(),
-            _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
-        });
+    public FilePath SystemLogsPath() => ResolvePath(
+        _ => MacOsPaths.SystemLogs(),
+        _ => LinuxPaths.SystemLogs(),
+        _ => WindowsPaths.SystemLogs());
 
-    public FilePath SystemCachePath() => ToFilePath(
-        operatingSystem switch
+    public int StaleProjectDays => 90;
+
+    public int StaleProjectMaxDepth => 6;
+
+    public FilePath SystemCachePath() => ResolvePath(
+        MacOsPaths.SystemCache,
+        LinuxPaths.SystemCache,
+        _ => WindowsPaths.SystemCache());
+
+    private FilePath ResolvePath(
+        Func<string, string> macOs,
+        Func<string, string> linux,
+        Func<string, string> windows) =>
+        ToFilePath(operatingSystem switch
         {
-            OperatingSystemType.MacOS => MacOsPaths.SystemCache(homeDirectory),
-            OperatingSystemType.Linux => LinuxPaths.SystemCache(homeDirectory),
-            OperatingSystemType.Windows => WindowsPaths.SystemCache(),
+            OperatingSystemType.MacOS => macOs(homeDirectory),
+            OperatingSystemType.Linux => linux(homeDirectory),
+            OperatingSystemType.Windows => windows(homeDirectory),
             _ => throw new PlatformNotSupportedException($"Unsupported operating system: {operatingSystem}")
         });
 
